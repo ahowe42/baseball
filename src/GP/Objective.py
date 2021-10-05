6285b99f21c78b54d99871aecb828f6299010111
ahowe42 <ahowe42@protonmail.com>
2021-10-05 21:41:38 +0100

    Updated TreeMetric() to include a few classification metrics, and added the TreeClassificationMetric() objective function.

--- src/GP/Objective.py
+++ src/GP/Objective.py
@@ -5,15 +5,15 @@ import ipdb
 import sys
 
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_percentage_error
-from sklearn.metrics import r2_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error, r2_score
+from sklearn.metrics import accuracy_score, f1_score
 
 sys.path.append('../')
 from util.Utils import *
 
-    
-    
+
+
 def TreeRegressionMetric(data, tree, feats, metric='RMSE', optimGoal=-1):
     '''
     For a given dataset and tree, evalute the tree on the
@@ -80,13 +80,77 @@ def TreeRegressionMetric(data, tree, feats, metric='RMSE', optimGoal=-1):
     return (metricVal, preds, linReg)
 
 
+def TreeClassificationMetric(data, tree, feats, metric='accuracy', optimGoal=-1):
+    '''
+    For a given dataset and tree, evalute the tree on the
+    dataset, and assess the classification relationship between
+    the results and the target in the data, returning the specified
+    metric. Metric choices include 'accuracy', 'F1', 'wF1', 
+    or you can pass in a callable with 'y_true' and 'y_pred' arguments.
+    :param data: dataframe of data; columns should include
+        'target', and 'X0', 'X1', ...
+    :param tree: evaluateable string function of a tree
+    :param feats: list of feature column names
+    :param metric: optional (default='accuracy') metric to compute
+    :param optimGoal: flag indicating what to do with the metric
+        (1 = maximize, -1 = minimize); this is only used to put a sign
+        on a returned np.inf, in the case of an error
+    :return metricVal: single-element tuple holding the value of the
+        specified metric from a linear fit between the tree results
+        and the target data column; if an error occurs np.inf is returned
+    :return preds: array-like of predictions using linear regression model
+    :return linReg: fit decision tree estimator
+    '''
+    
+    # append the dataframe to the column name
+    for feat in feats:
+        tree = tree.replace(feat, 'data.'+feat)
+        
+    # evaluate the tree function
+    treeRes = eval(tree)
+    if type(treeRes) is pd.Series:
+        # if the tree just encodes a series, have to get values
+        treeRes = treeRes.values
+    try:
+        treeRes = treeRes.reshape(-1, 1)
+    except AttributeError:
+        # if the tree just encodes a constant value, make an array
+        treeRes = np.array([treeRes]*len(data)).reshape(-1, 1)
+    
+    # regression between target and the tree results
+    try:
+        decTree = DecisionTreeClassifier()
+        decTree.fit(X=treeRes, y=data['target'].values)
+        preds = decTree.predict(X=treeRes)
+        # compute the metric
+        if metric == 'accuracy':
+            metricVal = accuracy_score(y_true=data['target'].values, y_pred=preds)
+        elif metric == 'F1':
+            metricVal = f1_score(y_true=data['target'].values, y_pred=preds)
+        elif metric == 'wF1':
+            metricVal = f1_score(y_true=data['target'].values, y_pred=preds, average='weighted')
+        elif not isinstance(metric, str):
+            metricVal = metric(y_true=data['target'].values, y_pred=preds)
+    except ValueError:
+        # nans or infs in the tree res, so just pass out np.inf
+        preds = [np.nan]*len(data)
+        metricVal = np.inf*optimGoal*-1
+    except Exception as err:
+        # don't know, but perhaps an error with the metric function
+        print('Unkonwn error: %s'%err)
+        preds = [np.nan]*len(data)
+        metricVal = np.inf*optimGoal*-1
+    
+    return (metricVal, preds, decTree)
+
+
 def TreeMetric(data, tree, feats, metric='RMSE', optimGoal=-1):
     '''
     For a given dataset and tree, evalute the tree on the
     dataset, and compute a specified metric between the target
     and the tree results data. Metric choices include 'RMSE',
-    'MSE', 'MAPE', 'R^2', or you can pass in a callable with
-    'y_true' and 'y_pred' arguments.
+    'MSE', 'MAPE', 'R^2', 'accurary', 'F1', 'wF1' or you can
+    pass in a callable with 'y_true' and 'y_pred' arguments.
     :param data: dataframe of data; columns should include
         'target', and 'X0', 'X1', ...
     :param tree: evaluateable string function of a tree
@@ -126,6 +190,12 @@ def TreeMetric(data, tree, feats, metric='RMSE', optimGoal=-1):
             metricVal = mean_absolute_percentage_error(y_true=data['target'].values, y_pred=preds)
         elif metric == 'R^2':
             metricVal = r2_score(y_true=data['target'].values, y_pred=preds)
+        elif metric == 'accuracy':
+            metricVal = accuracy_score(y_true=data['target'].values, y_pred=preds)
+        elif metric == 'F1':
+            metricVal = f1_score(y_true=data['target'].values, y_pred=preds)
+        elif metric == 'wF1':
+            metricVal = f1_score(y_true=data['target'].values, y_pred=preds, average='weighted')
         elif not isinstance(metric, str):
             metricVal = metric(y_true=data['target'].values, y_pred=preds)
     except ValueError:
